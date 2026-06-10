2c749c2ae51b2ae40192056ddec7ead44e0e0475
luanzh000 <luanfelipedasilvaleonel@gmail.com>
2026-06-10 08:07:27 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// CLASSE MÃE
 class Voo {
     constructor(codigo, origem, destino) {
         this.codigo = codigo;
@@ -108,3 +107,66 @@ function embarcarCarga() {
 }
 
 atualizarPainel();
+
+
+new Voo("GEN-000");
+
+class Animal {
+    constructor() {
+        console.log(new.target.name);
+    }
+}
+
+class Cachorro extends Animal {}
+
+new Animal();   // Animal
+new Cachorro(); // Cachorro
+
+class Voo {
+    constructor(codigo) {
+
+        if (new.target === Voo) {
+            throw new Error("A classe Voo é abstrata e não pode ser instanciada diretamente.");
+        }
+
+        this.codigo = codigo;
+    }
+
+    calcularTaxaEmbarque() {
+        return 50;
+    }
+}
+
+new Voo("GEN-000");
+
+new VooCarga("CARGO-99", 500);
+
+calcularTaxaEmbarque()
+
+class VooComercial extends Voo {
+    constructor(codigo, qtdPassageiros) {
+        super(codigo);
+        this.qtdPassageiros = qtdPassageiros;
+    }
+
+    calcularTaxaEmbarque() {
+        return this.qtdPassageiros * 50.00;
+    }
+}
+
+class VooCarga extends Voo {
+    constructor(codigo, toneladas) {
+        super(codigo);
+        this.toneladas = toneladas;
+    }
+
+    calcularTaxaEmbarque() {
+        return this.toneladas * 120.00;
+    }
+}
+
+let comercial = new VooComercial("COM-101", 200);
+console.log(comercial.calcularTaxaEmbarque());
+
+let cargueiro = new VooCarga("CARGO-99", 500);
+console.log(cargueiro.calcularTaxaEmbarque());
